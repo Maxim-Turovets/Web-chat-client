79faa341a180a757e83c89ea79b6c31b8f5bea81
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2019-03-20 14:48:48 +0200

    Update clientLogic.js

--- client/clientLogic.js
+++ client/clientLogic.js
@@ -1,38 +1,41 @@
 // section
-const connectInfo = document.querySelector(".connect-info");
-const generalInfo = document.querySelector(".general-info");
-const chatWindow = document.querySelector(".container-chat-form");
-const userInfo = document.querySelector(".user-info");
-const interlocutorInfo = document.querySelector(".interlocutor-info");
+const containerChatType = document.getElementById("containerChatType");
+const containerSetGender = document.getElementById("containerSetGender");
+const containerSetAge = document.getElementById("containerSetAge");
+const containerSetName = document.getElementById("containerSetName");
+const containerSettingsVoiceMessage = document.getElementById("containerSettingsVoiceMessage");
+const containerLoading = document.getElementById("containerLoading");
+const containerPartnersGender = document.getElementById("containerPartnersGender");
+const containerSetPartnerAge = document.getElementById("containerSetPartnerAge");
+
 const contaonerMessage = document.querySelector(".container-messages");
-const loading = document.getElementById("loading");
+const messageSend =document.getElementById("message-send");
 
 // button
-const btnPair = document.getElementById("btnPair");
+const btnPrivate = document.getElementById("btnPrivate");
 const btnGeneral = document.getElementById("btnGeneral");
-const btnGeneralConnect = document.getElementById("btnGeneralConnect");
-const btnSend = document.getElementById("btnSend");
-const btnMale = document.getElementById("btnMale");
-const btnFemale = document.getElementById("btnFemale");
-const btnNext = document.getElementById("btnNext");
-const btnInterlocutorMale = document.getElementById("btnInterlocutorMale");
-const btnInterlocutorFemale = document.getElementById("btnInterlocutorFemale");
+const btnSetNameNext = document.getElementById("btnSetNameNext");
+const btnSetGenderMale = document.getElementById("btnSetGenderMale");
+const btnSetGenderFemale= document.getElementById("btnSetGenderFemale");
+const btnSetAgeNext = document.getElementById("btnSetAgeNext");
+const btnVoiceMessageYes = document.getElementById("btnVoiceMessageYes");
+const btnVoiceMessageNo = document.getElementById("btnVoiceMessageNo");
+const btnPartnerMale = document.getElementById("btnPartnerMale");
+const btnPartnerFemale = document.getElementById("btnPartnerFemale");
 const btnSearch = document.getElementById("btnSearch");
-const BtnGenChat = document.querySelector(".container-btn-general-chat");
-const BtnGirl = document.querySelector(".container-btn-im-girl");
-const BtnBoy = document.querySelector(".container-btn-im-boy");
 
 //input
-const inpGeneralName = document.getElementById("inpGeneralName");
-const inpMessage = document.getElementById("inpMessage");
-const inpUserName = document.getElementById("inpUserName");
-const inpAge = document.getElementById("inpAge");
-const inpInterlocutorAgeFrom = document.getElementById("inpInterlocutorAgeFrom");
-const inpInterlocutorAgeTo = document.getElementById("inpInterlocutorAgeTo");
-const name = document.querySelector(".input-name");
-const text = document.querySelector(".input-text");
+const inpSetName = document.getElementById("inpSetName");
+const inpSetAge = document.getElementById("inpSetAge");
+const inpPartnersAgeFrom = document.getElementById("inpPartnersAgeFrom");
+const inpPartnersAgeTo = document.getElementById("inpPartnersAgeTo");
 
-const con = new WebSocket("ws://77.47.224.135:8080/sock/chat");
+const inpText = document.getElementById("inpText");
+
+
+
+const text = document.querySelector(".input-text");
+const con = new WebSocket("ws://localhost:8080/sock/chat");
 
 // json structure
 let UserInfo={
@@ -55,56 +58,28 @@ let InterlocutorInfo={
     ageTo:""
 }
 
+let Message ={
+    objectType:"Message",
+    name:"",
+    text:""
+}
 
 
-btnSend.addEventListener("click", e => btnSendPress());
 btnGeneral.addEventListener("click", e => btnGenChatPress());
-btnPair.addEventListener("click", e => btnPairChatPress());
-btnGeneralConnect.addEventListener("click", e => btnGeneralConnectPress());
-btnMale.addEventListener("click", e => btnMalePress());
-btnFemale.addEventListener("click", e => btnFemalePress());
-btnNext.addEventListener("click", e => btnNextPress());
-btnInterlocutorMale.addEventListener("click", e => btnInterlocutorMalePress());
-btnInterlocutorFemale.addEventListener("click", e => btnInterlocutorFemalePress());
+btnPrivate.addEventListener("click", e => btnPrivatePress());
+btnSetNameNext.addEventListener("click", e => btnSetNamePress());
+btnSetGenderMale.addEventListener("click", e => btnSetGenderMalePress());
+btnSetGenderFemale.addEventListener("click", e => btnSetGenderFemalePress());
+btnSetAgeNext.addEventListener("click", e => btnSetAgeNextPress());
+btnVoiceMessageYes.addEventListener("click", e => btnVoiceMessageYesPress());
+btnVoiceMessageNo.addEventListener("click", e => btnVoiceMessageNoPress());
+btnPartnerMale.addEventListener("click", e => btnPartnerMalePress());
+btnPartnerFemale.addEventListener("click", e => btnPartnerFemalePress());
 btnSearch.addEventListener("click", e => btnSearchPress());
 
-// text.addEventListener("keyup", function (event) {
-//     event.preventDefault();
-//     if (event.keyCode === 13) {
-//         btnSendPress();
-//     }
-// });
 
 
-// const createOtherMessage = text => {
-//     let jsonRequest = JSON.parse(text);
-//     if (jsonRequest.text === "none" && jsonRequest.authkey === "general"&&jsonRequest.name!="none") {
-//         createAlertNewUser(jsonRequest.name);
-//     }
-//     else {
-//         const d = document.createElement('div');
-//         let html = "";
-//         html += "<div class=\"row\">";
-//         html += "<div class=\"col-12\">";
-//         if (jsonRequest.name === "") {
-//             html += "<div class=\"name float-left\">" + "Anonymous" + "</div>";
-//         } else {
-//             html += "<div class=\"name float-left\">" + jsonRequest.name + "</div>";
-//         }
-//         html += "</div></div>";
-//         html += "<div class=\"row\">";
-//         html += "<div class=\"col-sm-12\">";
-//         html += "<div class=\"other-messages float-left\" id=\"message-" + messageCount.toString() + "\">" + jsonRequest.text + "";
-//
-//         html += "</div></div>";
-//         d.innerHTML = html;
-//         document.querySelector(".container-messages").appendChild(d);
-//         let audio = new Audio('message.mp3');
-//         audio.volume = 1;
-//         audio.play();
-//     }
-//     document.location.href = "#message-" + messageCount.toString();
-// };
+
 
 con.onopen = () => {
     console.log('connected');
@@ -114,42 +89,39 @@ con.onclose = () => {
 };
 
 con.onmessage = event => {
-    // console.log(event.data);
     if(event.data==="created")
     {
-        loading.style.display="none";
-        interlocutorInfo.style.display="none";
+        containerLoading.style.display="none";
         contaonerMessage.style.display="block";
-        //console.log(event.data);
         createAlertNewUser();
+        messageSend.style.display="block";
+
     }
-    //createOtherMessage(event.data);
+    createOtherMessage(event.data);
 };
 
 function btnSendPress(qualifiedName, value) {
-    console.log("cl");
-    let json ={
-        name:inpGeneralName.value,
-        text:inpMessage.value
-    }
-    con.send(JSON.stringify(json));
+    Message.name = UserInfo.name;
+    Message.text = inpText.value;
+    con.send(JSON.stringify(Message));
     createMyMessage();
-    clearInput();
 }
 
 function btnGenChatPress(qualifiedName, value) {
     ConnectInfo.chatType="general";
     con.send(JSON.stringify(ConnectInfo));
-    connectInfo.style.display = "none";
-    generalInfo.style.display="block";
+    containerChatType.style.display = "none";
+    containerSetName.style.display="block";
+    //generalInfo.style.display="block";
 }
 
-function btnPairChatPress(qualifiedName, value) {
-    ConnectInfo.chatType="pair";
-    con.send(JSON.stringify(ConnectInfo));
-    connectInfo.style.display = "none";
-    userInfo.style.display="block";
-}
+function btnPrivatePress(qualifiedName, value) {
+       ConnectInfo.chatType="pair";
+       con.send(JSON.stringify(ConnectInfo));
+       containerChatType.style.display = "none";
+       containerSetName.style.display="block";
+       //userInfo.style.display="block";
+ }
 
 
 function createMyMessage() {
@@ -158,226 +130,84 @@ function createMyMessage() {
     let html = "";
     html += "<div class=\"row\">";
     html += "<div class=\"col-12\">";
-    html += "<div class=\"name float-right\">" + generalInfo.value + "</div>";
+    html += "<div class=\"name float-right\">" + Message.name + "</div>";
     html += "</div></div>";
     html += "<div class=\"row\">";
     html += "<div class=\"col-sm-12\">";
-    html += "<div class=\"my-messages float-right\" id=\"message-" + messageCount.toString() + "\">" + safetyMessage + "</div>";
+    html += "<div class=\"my-messages float-right\" id=\"message-"+ "\">" + Message.text + "</div>";
     html += "</div></div>";
     d.innerHTML = html;
     document.querySelector(".container-messages").appendChild(d);
 }
-//
-// function clearInput() {
-//     document.getElementById("text-input").value = "";
-// }
-//
+
 function createAlertNewUser() {
     const d = document.createElement('div');
     let html = "";
     html += "<div class=\"row\">";
     html += "<div class=\"col-sm-12\">";
-    html += "<div class=\"new-user text-center\" id=\"message-"  + "\">" + " joined the chat</div></div>";
+    html += "<div class=\"new-user text-center\" id=\"message-"  + "\">" + " interlocutor joined the chat</div></div>";
     html += "</div>";
     d.innerHTML = html;
     document.querySelector(".container-messages").appendChild(d);
 }
 
 
-
-
-function  btnGeneralConnectPress(){
-    generalInfo.style.display="none";
-    chatWindow.style.display="block";
-}
-
-function btnMalePress() {
+function btnSetGenderMalePress() {
     UserInfo.gender="male";
+    containerSetGender.style.display="none";
+    containerSetAge.style.display="block";
 }
-function btnFemalePress() {
+function btnSetGenderFemalePress() {
     UserInfo.gender="female";
+    containerSetGender.style.display="none";
+    containerSetAge.style.display="block";
 }
 
-function btnVoiceMessagePress() {
-    UserInfo.voiceMessage=true;
+
+function btnSetAgeNextPress() {
+    UserInfo.age = inpSetAge.value;
+    containerSetAge.style.display="none";
+    containerSettingsVoiceMessage.style.display="block";
 }
 
-function btnNextPress() {
-    UserInfo.name = inpUserName.value;
-    UserInfo.age = inpAge.value;
-    con.send(JSON.stringify(UserInfo));
-    userInfo.style.display="none";
-    interlocutorInfo.style.display="block";
-}
 
-function btnInterlocutorMalePress() {
-    InterlocutorInfo.gender ="male";
-}
-function btnInterlocutorFemalePress() {
-    InterlocutorInfo.gender ="female";
-}
 function btnSearchPress() {
-    InterlocutorInfo.ageFrom = inpInterlocutorAgeFrom.value;
-    InterlocutorInfo.ageTo = inpInterlocutorAgeTo.value;
+    InterlocutorInfo.ageFrom = inpPartnersAgeFrom.value;
+    InterlocutorInfo.ageTo = inpPartnersAgeTo.value;
     con.send(JSON.stringify(InterlocutorInfo));
-    loading.style.display="block";
-    interlocutorInfo.style.display="none";
+    containerSetPartnerAge.style.display="none";
+    containerLoading.style.display="block";
+}
+
+function btnSetNamePress() {
+    UserInfo.name = inpSetName.value;
+    containerSetName.style.display = "none";
+    containerSetGender.style.display = "block";
+}
+
+function btnVoiceMessageYesPress() {
+    UserInfo.voiceMessage=true;
+    containerSettingsVoiceMessage.style.display="none";
+    containerPartnersGender.style.display="block";
+    con.send(JSON.stringify(UserInfo));
+}
+
+function btnVoiceMessageNoPress() {
+    UserInfo.voiceMessage=  false;
+    containerSettingsVoiceMessage.style.display="none";
+    containerPartnersGender.style.display="block";
+    con.send(JSON.stringify(UserInfo))
 
 }
 
+function btnPartnerMalePress() {
+    InterlocutorInfo.gender="male";
+    containerPartnersGender.style.display="none";
+    containerSetPartnerAge.style.display="block";
+}
 
-
-
-
-
-// const BtnSend = document.querySelector(".container-btn-send");
-// const BtnGenChat = document.querySelector(".container-btn-general-chat");
-// const BtnPrivChat = document.querySelector(".container-btn-private-chat");
-// const BtnGirl = document.querySelector(".container-btn-im-girl");
-// const BtnBoy = document.querySelector(".container-btn-im-boy");
-// const name = document.querySelector(".input-name");
-// const text = document.querySelector(".input-text");
-// const con = new WebSocket("ws://77.47.224.135:8080/sock/chat");
-// let messageCount = 0;
-// let gender = "none";
-// let chatType = "none";
-//
-//
-// BtnSend.addEventListener("click", e => btnSendPress());
-// BtnGenChat.addEventListener("click", e => btnGenChatPress());
-// BtnPrivChat.addEventListener("click", e => btnPrivateChatPress());
-// BtnGirl.addEventListener("click", e => btnGirlPress());
-// BtnBoy.addEventListener("click", e => btnBoyPress());
-//
-// text.addEventListener("keyup", function (event) {
-//     event.preventDefault();
-//     if (event.keyCode === 13) {
-//         btnSendPress();
-//     }
-// });
-//
-//
-// const createOtherMessage = text => {
-//     let jsonRequest = JSON.parse(text);
-//     if (jsonRequest.text === "none" && jsonRequest.authkey === "general"&&jsonRequest.name!="none") {
-//         createAlertNewUser(jsonRequest.name);
-//     }
-//     else {
-//         const d = document.createElement('div');
-//         let html = "";
-//         html += "<div class=\"row\">";
-//         html += "<div class=\"col-12\">";
-//         if (jsonRequest.name === "") {
-//             html += "<div class=\"name float-left\">" + "Anonymous" + "</div>";
-//         } else {
-//             html += "<div class=\"name float-left\">" + jsonRequest.name + "</div>";
-//         }
-//         html += "</div></div>";
-//         html += "<div class=\"row\">";
-//         html += "<div class=\"col-sm-12\">";
-//         html += "<div class=\"other-messages float-left\" id=\"message-" + messageCount.toString() + "\">" + jsonRequest.text + "";
-//
-//         html += "</div></div>";
-//         d.innerHTML = html;
-//         document.querySelector(".container-messages").appendChild(d);
-//         let audio = new Audio('message.mp3');
-//         audio.volume = 1;
-//         audio.play();
-//     }
-//     // document.location.href = "#message-" + messageCount.toString();
-// };
-//
-// con.onopen = () => {
-//     console.log('connected');
-// };
-// con.onclose = () => {
-//     console.log('closed');
-// };
-//
-// con.onmessage = event => {
-//     createOtherMessage(event.data);
-// };
-//
-// function btnSendPress(qualifiedName, value) {
-//     let json = "";
-//     let safetyMessage = text.value.replace(/[<]/g, "&lt");
-//
-//     json += "{\"name\":\"" + name.value + "\",\"text\":\"" + safetyMessage + "\",\"authkey\":\"" + chatType +"\"}";
-//     // con.send(json);
-//     console.log(json);
-//     createMyMessage();
-//     clearInput();
-// }
-//
-// function btnGenChatPress(qualifiedName, value) {
-//     document.querySelector(".container-login-form").setAttribute("style", "display:none");
-//     document.querySelector(".container-chat-form").removeAttribute("style");
-//     chatType = "general";
-//     createAlertNewUser(name.value);
-//     newUserLogin();
-// }
-//
-// function btnPrivateChatPress(qualifiedName, value) {
-//     document.querySelector(".container-login-form").setAttribute("style", "display:none");
-//     document.querySelector(".container-chat-form").removeAttribute("style");
-//     chatType = "pair";
-//     createAlertNewUser(name);
-//     newUserLogin();
-// }
-//
-// function newUserLogin() { //    send message about new user login
-//     let json = {
-//         name:name.value,
-//         text:"none",
-//         authkey:"general"
-//     };
-//     con.send(JSON.stringify(json));
-// }
-//
-//
-// function createMyMessage() {
-//     messageCount++;
-//
-//     const d = document.createElement('div');
-//     let safetyMessage = text.value.replace(/[<]/g, "&lt");
-//     let html = "";
-//     html += "<div class=\"row\">";
-//     html += "<div class=\"col-12\">";
-//     html += "<div class=\"name float-right\">" + name.value + "</div>";
-//     html += "</div></div>";
-//     html += "<div class=\"row\">";
-//     html += "<div class=\"col-sm-12\">";
-//     html += "<div class=\"my-messages float-right\" id=\"message-" + messageCount.toString() + "\">" + safetyMessage + "</div>";
-//     html += "</div></div>";
-//     d.innerHTML = html;
-//     document.querySelector(".container-messages").appendChild(d);
-//     document.location.href = "#message-" + messageCount.toString();
-// }
-//
-// function clearInput() {
-//     document.getElementById("text-input").value = "";
-// }
-//
-// function createAlertNewUser(n) {
-//     const d = document.createElement('div');
-//     messageCount++;
-//     let html = "";
-//     html += "<div class=\"row\">";
-//     html += "<div class=\"col-sm-12\">";
-//     html += "<div class=\"new-user text-center\" id=\"message-" + messageCount.toString() + "\">" + n + " joined the chat</div></div>";
-//     html += "</div>";
-//     d.innerHTML = html;
-//     document.querySelector(".container-messages").appendChild(d);
-//     let audio = new Audio('message.mp3');
-//     audio.volume = 1;
-//     audio.play();
-//     document.location.href = "#message-" + messageCount.toString();
-// }
-//
-// function btnGirlPress() {
-//     gender = "girl";
-// }
-//
-// function btnBoyPress() {
-//     gender = "boy";
-// }
+function btnPartnerFemalePress() {
+    InterlocutorInfo.gender="female";
+    containerPartnersGender.style.display="none";
+    containerSetPartnerAge.style.display="block";
+}
